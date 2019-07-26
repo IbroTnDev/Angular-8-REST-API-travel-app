990e90ebb7ccfdc8d0ddc9cc4c09b45c76c5a9f1
ibrahim arbi <ibrahim.arbi@frantic.com>
2019-07-26 16:26:52 +0300

    create fligh search result components + add css styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FlightsComponent } from './components/flights/flights.component';
+import { FlightSearchResultComponent } from './components/flight-search-result/flight-search-result.component';
 
 const routes: Routes = [
   { path: '', component: FlightsComponent },
-  //{ path: 'flight_search_result', component: FlightSearchResultComponent },
+  { path: 'flight_search_result', component: FlightSearchResultComponent }
 ];
 
 @NgModule({
---
+++ src/app/components/flight-search-result/flight-search-result.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { TypeaheadMatch, ModalDirective, BsModalService } from 'ngx-bootstrap';
+import { Router } from '@angular/router';
+import { FlightService } from '../../services/flight.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Flights } from 'src/app/interfaces/flights';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-flight-search-result',
+  templateUrl: './flight-search-result.component.html',
+  styleUrls: ['./flight-search-result.component.scss']
+})
+export class FlightSearchResultComponent {
+  flightHeader: any;
+  flights: [];
+  ngSwitch: any;
+
+constructor(private flightService: FlightService, private router: Router, private spinner: NgxSpinnerService) {
+    this.flights = JSON.parse(localStorage.getItem('flight'));
+    this.flightHeader = JSON.parse(localStorage.getItem('flightHeader'));
+   }
+
+  formateDateMS(checkinDate) {
+    const msec = Date.parse(checkinDate);
+    const d = moment(msec).format('MMM DD, YYYY');
+    if (d === 'Invalid date') {
+        return checkinDate;
+    }
+    return d;
+  }
+
+  sort() {}
+
+  formatTime3(date: number) {
+    const d = moment.unix(date).format('HH:mm');
+    return d;
+  }
+  formatDate2(date: number) {
+    const d = moment.unix(date).format('MMM DD, YYYY HH:mm');
+    return d;
+  }
+
+}
